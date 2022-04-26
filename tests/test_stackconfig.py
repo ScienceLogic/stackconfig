4a0384a0ca61045f29d737978a5854efbf1ff62d
Carme, Pamy <pamy.carme@sciencelogic.com>
2022-04-26 13:36:22 -0400

    fix: mock unit test jic docker is not installed
    Signed-off-by: Carme, Pamy <pamy.carme@sciencelogic.com>

--- tests/test_stackconfig.py
+++ tests/test_stackconfig.py
@@ -1,12 +1,20 @@
-import os
-
 import pytest
 
 from stackconfig.stackconfig import StackConfigCompose
 from stackconfig.utils.jinja2_utils import render_jijnja2_compose
 
 
-def test_merge_compose_files():
+@pytest.fixture()
+def mock_success_subprocess(mocker):
+    mocker.patch('subprocess.getoutput', return_value={"Additional property z_dummy is not allowed": "test"})
+
+
+@pytest.fixture()
+def mock_error_subprocess(mocker):
+    mocker.patch('subprocess.getoutput', return_value={"Error": "test"})
+
+
+def test_merge_compose_files(mock_success_subprocess):
     override_file = "tests/example_override.yml"
     templates = render_jijnja2_compose(
         [override_file], data_file="tests/data_example.yml"
@@ -15,33 +23,35 @@ def test_merge_compose_files():
         ["tests/example_compose.yml"] + templates, "/tmp/temp_result.yml"
     )
     c.merge_stack_compose()
-    assert c.compose_dict["version"] == "3.3"
+    assert c.compose_dict["version"] == "3.8"
+    assert "deploy" in c.compose_dict["services"]["ui"]
+    assert "placement" in c.compose_dict["services"]["ui"]["deploy"]
+    assert "max_replicas_per_node" in c.compose_dict["services"]["ui"]["deploy"]["placement"]
+    assert c.compose_dict["services"]["ui"]["deploy"]["placement"]["max_replicas_per_node"] == 1
 
 
-def test_merge_compose_files_invalid_syntax():
+def test_merge_compose_files_invalid_syntax(mock_success_subprocess):
     override_file = "tests/example_override_invalid.yml"
     with pytest.raises(Exception) as err:
         templates = render_jijnja2_compose(
             [override_file], data_file="tests/data_example.yml"
         )
-        c = StackConfigCompose(
-            ["tests/example_compose.yml"] + templates, "/tmp/temp_result.yml"
-        )
-        c.merge_stack_compose()
     assert f"Please be sure the template {override_file} is valid" in str(err)
 
 
-@pytest.mark.parametrize("version, version_result", [(None, "3.3"), ("3.8", "3.8")])
-def test_merge_compose_files_invalid(version, version_result):
+@pytest.mark.parametrize("version", [(None), ("3.9")])
+def test_merge_compose_files_invalid(version, mock_success_subprocess):
     c = StackConfigCompose(
         ["tests/example_compose.yml"], "/tmp/temp_result_invalid.yml", version
     )
     c.merge_stack_compose()
-    assert c.compose_dict["version"] == version_result
+    if not version:
+        version = "3.8"
+    assert c.compose_dict["version"] == version
     assert "depends_on" not in c.compose_dict["services"]["api"]
 
 
-def test_merge_compose_files_syntax_error():
+def test_merge_compose_files_syntax_error(mock_success_subprocess):
     with open("/tmp/invalid-compose.yml", "+w") as file:
         file.writelines("{}\ntests: test_value".format("test"))
     with pytest.raises(Exception) as exc:
@@ -51,3 +61,16 @@ def test_merge_compose_files_syntax_error():
         )
         c.merge_stack_compose()
     assert "mapping values are not allowed here" in str(exc)
+
+
+def test_merge_compose_files_invalid_syntax_compose_validation(mock_error_subprocess):
+    override_file = "tests/example_override_invalid2.yml"
+    with pytest.raises(Exception) as err:
+        templates = render_jijnja2_compose(
+            [override_file], data_file="tests/data_example.yml"
+        )
+        c = StackConfigCompose(
+            ["tests/example_compose.yml"] + templates, "/tmp/temp_result.yml"
+        )
+        c.merge_stack_compose()
+    assert f"services.service_custom.deploy.replicas contains an invalid type" in str(err)
